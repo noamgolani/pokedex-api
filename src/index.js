d93c7ebd88fddc261b241a279f99f5df2e75483b
noamgolani <noam.golani@gmail.com>
2021-10-21 16:40:12 +0300

    added cors

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const pokemonRouter = require("./routes/pokemonRouter");
 const userRouter = require("./routes/userRouter");
 const { userHandler } = require("./middleware/userHandler");
 const { errorHandler } = require("./middleware/errorHandler");
+
+const cors = require(cors);
 const express = require("express");
 const app = express();
 
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/pokemon", userHandler, pokemonRouter);
