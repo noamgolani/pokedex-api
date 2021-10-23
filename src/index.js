2b80ec47652ac64ba19964ca8615c93645696a63
noamgolani <noam.golani@gmail.com>
2021-10-23 17:52:06 +0300

    more checking

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || "127.0.0.1";
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use("/pokemon", userHandler, pokemonRouter);
 app.use("/user", userRouter);
 app.get("/", function (req, res) {
-  console.log(fs.readdirSync(path.resolve("./front")));
+  console.log(fs.readdirSync(path.resolve("./")));
   res.sendFile(path.resolve("./front/dist/index.html"));
 });
 app.use("/", express.static(path.resolve("./front/dist")));
@@ -25,5 +26,5 @@ app.use("/", express.static(path.resolve("./front/dist")));
 app.use(errorHandler);
 
 app.listen(port, function () {
-  console.log(`Listening on: http://localhost:${port}`);
+  console.log(`Listening on: ${host}:${port}`);
 });
